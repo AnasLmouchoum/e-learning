d07eae05206f7fe451152a1cf6abf37b03b75da9
AnasLmouchoum <95775026+AnasLmouchoum@users.noreply.github.com>
2024-07-21 19:42:52 +0100

    initial commit

---
+++ e-learning-api/src/test/java/ao/e/learning/elearningapi/ELearningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.e.learning.elearningapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
